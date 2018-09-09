967216435deb039e08308ca11163e9ba1925e455
dwolfe1987@gmail.com <dwolfe1987@gmail.com>
2018-09-09 19:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-hapyHolidayTo(holiday, name) {
-  
+happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
